82aba9984b2db98984a0e7aa40944ba0d525133c
Pedro <santiagvs@proton.me>
2024-10-06 20:45:46 -0300

    feat(posts): creating Posts controller, model and route
    Adds to the app the MVC for Posts ecosystem, especially adding and editing a post:

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,14 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{name: "Alice", email: "alice@example.com"},
+      %{name: "Bob", email: "bob@example.com"},
+      %{name: "Charlie", email: "charlie@example.com"}
+    ]
+
+    json(conn, %{users: users})
+  end
 end
