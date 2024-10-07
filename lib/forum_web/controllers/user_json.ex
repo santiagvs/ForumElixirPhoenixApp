5503d455268e57009406ff279f290ff3d3183d2e
Pedro <santiagvs@proton.me>
2024-10-07 07:50:23 -0300

    feat(users): now posts have to reference a user_id; users return all their posts
    Implements the association between Posts and Users in the API, returning all posts made by an user within the payload, and requiring the new post to have an user associated to it.

--- lib/forum_web/controllers/user_json.ex
+++ lib/forum_web/controllers/user_json.ex
@@ -1,25 +1,35 @@
 defmodule ForumWeb.UserJSON do
   alias Forum.Accounts.User
+  alias Forum.Posts.Post
 
   @doc """
   Renders a list of users.
   """
   def index(%{users: users}) do
-    %{data: for(user <- users, do: data(user))}
+    %{users: for(user <- users, do: data(user))}
   end
 
   @doc """
   Renders a single user.
   """
   def show(%{user: user}) do
-    %{data: data(user)}
+    %{user: data(user)}
   end
 
   defp data(%User{} = user) do
     %{
       id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      posts: for(post <- user.posts, do: post_data(post)),
+    }
+  end
+
+  defp post_data(%Post{} = post) do
+    %{
+      id: post.id,
+      title: post.title,
+      body: post.body,
     }
   end
 end
